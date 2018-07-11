b84f75b8f8b371e4aa105e7fd47703ad42e08d13
陈贺 <he.chen@lanmaoly.com>
2018-07-11 16:59:25 +0800

    Hystrix断路由

--- config-client/src/main/java/com/chenhe/ConfigClientApplication.java
+++ config-client/src/main/java/com/chenhe/ConfigClientApplication.java
@@ -8,6 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +28,10 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 //开启Feign的功能
 @EnableFeignClients
+//开启断路由
+@EnableHystrix
+//开启图形界面(访问 http://localhost:<port>/hystrix)
+@EnableHystrixDashboard
 public class ConfigClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
--- config-client/src/main/java/com/chenhe/service/RibbonService.java
+++ config-client/src/main/java/com/chenhe/service/RibbonService.java
@@ -1,5 +1,6 @@
 package com.chenhe.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -15,7 +16,14 @@ public class RibbonService {
     @Autowired
     RestTemplate restTemplate;
 
+    //当访问不到provider的时候,触发指定方法
+    @HystrixCommand(fallbackMethod ="hiError" )
     public String hiService(String name) {
         return restTemplate.getForObject("http://hello-service/h?msg="+name,String.class);
+    }
+
+
+    public String hiError(String name){
+        return "服务器挂了!!!";
     }
 }
